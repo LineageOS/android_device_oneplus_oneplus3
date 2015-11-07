6a883ae46c403db5bfc55072d126b638efac4368
Scott Mertz <scott@cyngn.com>
2015-11-06 17:44:53 -0800

    gps: fix build for OSS builds
    gps: Correct forward declaration in proprietary definitions
    
    Fix gps runtime error
    
    The qcom prebuilt binary relies on FlpExtLocation_s,
    so we cannot change it to FlpExtLocation.
    This patch fixes it.
    
    ether: gps: fix remove trailing space
    
    In setXtraUserAgent,
    size_t is unsigned and never being negative.
    Will cause segmentation fault, if string is all space.
    
    Change-Id: I1cf1f956943b0739640afe909954ade6921e28a1

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -47,8 +47,7 @@ LOCAL_COPY_HEADERS:= \
     gps_extended_c.h \
     gps_extended.h \
     loc_core_log.h \
-    LocAdapterProxyBase.h \
-    fused_location_extended.h
+    LocAdapterProxyBase.h
 
 LOCAL_PRELINK_MODULE := false
 
