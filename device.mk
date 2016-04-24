c7b6808034acf38433132b6d0345e1a5a97762c2
Patrick Lower <devvortex@gmail.com>
2016-04-24 22:58:35 +0100

    oneplus3: add doze package
    Taken from yu lettuce
    Includes the following patches
    
    YUDoze: Add service for using proximity sensor for wake-up
    Author: Patrick Lower <devvortex@gmail.com>
    Date:   Mon May 11 16:14:17 2015 -0400
    
    YUDoze: Set gesture to disabled by default
    Author: Arne Coucheron <arco68@gmail.com>
    Date:   Wed May 20 00:56:19 2015 +0200
    
    YUDoze: Consolidate doze settings
    Author: Christopher R. Palmer <crpalmer@gmail.com>
    Date:   Sat May 23 11:42:49 2015 -0700
    
    YUDoze: fix defaults
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Sat May 23 18:29:13 2015 -0400
    
    YUDoze: Add back button to action bar
    Author: luca020400 <luca.stefani.ge1@gmail.com>
    Date:   Sun May 24 15:47:17 2015 -0700
    
    YUDoze: add proximity wake
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Mon May 25 12:54:00 2015 -0400
    
    YUDoze: restore proximity wake preference on boot
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Sat Jul 4 13:44:53 2015 -0400
    
    YUDozeService: Prevent a pm wakeup with a timestamp in the future
    Author: Brandon McAnsh <brandon.mcansh@gmail.com>
    Date:   Fri Jul 24 15:40:45 2015 -0400
    
    YUDozeService: properly fix future timestamp error
    Author: Patrick Lower <devvortex@gmail.com>
    Date:   Wed Aug 5 10:51:03 2015 -0400
    
    YUDoze: fix typo
    Author: Dave Daynard <nardholio@gmail.com>
    Date:   Tue Sep 8 22:56:10 2015 -0400
    
    YUDoze: Use ScreenType util from cmsdk
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Sun Dec 13 02:51:09 2015 -0500
    
    YUDOze: Remove summary for YUDoze
    Author: TheStrix <parthbhatia98@gmail.com>
    Date:   Sun Dec 13 07:25:36 2015 -0500
    
    Change-Id: Ia7e668622eecf4355fd55dc9cda7ca05a6494b93

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    OneplusDoze
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
---
+++ doze/src/com/cyanogenmod/settings/doze/DozeSettings.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.doze;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.cyanogenmod.internal.util.ScreenType;
+
+public class DozeSettings extends PreferenceActivity {
+
+    private static final String KEY_AMBIENT_DISPLAY_ENABLE = "ambient_display_enable";
+    private static final String KEY_HAND_WAVE = "gesture_hand_wave";
+    private static final String KEY_GESTURE_POCKET = "gesture_pocket";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_wake_enable";
+
+    private SwitchPreference mAmbientDisplayPreference;
+    private SwitchPreference mHandwavePreference;
+    private SwitchPreference mPocketPreference;
+    private SwitchPreference mProximityWakePreference;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.gesture_panel);
+        boolean dozeEnabled = isDozeEnabled();
+        mAmbientDisplayPreference =
+            (SwitchPreference) findPreference(KEY_AMBIENT_DISPLAY_ENABLE);
+        // Read from DOZE_ENABLED secure setting
+        mAmbientDisplayPreference.setChecked(dozeEnabled);
+        mAmbientDisplayPreference.setOnPreferenceChangeListener(mAmbientDisplayPrefListener);
+        mHandwavePreference =
+            (SwitchPreference) findPreference(KEY_HAND_WAVE);
+        mHandwavePreference.setEnabled(dozeEnabled);
+        mHandwavePreference.setOnPreferenceChangeListener(mProximityListener);
+        mPocketPreference =
+            (SwitchPreference) findPreference(KEY_GESTURE_POCKET);
+        mPocketPreference.setEnabled(dozeEnabled);
+        mProximityWakePreference =
+            (SwitchPreference) findPreference(KEY_PROXIMITY_WAKE);
+        mProximityWakePreference.setOnPreferenceChangeListener(mProximityListener);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // If running on a phone, remove padding around the listview
+        if (!ScreenType.isTablet(this)) {
+            getListView().setPadding(0, 0, 0, 0);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean enableDoze(boolean enable) {
+        return Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, enable ? 1 : 0);
+    }
+
+    private boolean isDozeEnabled() {
+        return Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.DOZE_ENABLED, 1) != 0;
+    }
+
+    private Preference.OnPreferenceChangeListener mAmbientDisplayPrefListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            boolean enable = (boolean) newValue;
+            boolean ret = enableDoze(enable);
+            if (ret) {
+                mHandwavePreference.setEnabled(enable);
+                mPocketPreference.setEnabled(enable);
+            }
+            return ret;
+        }
+    };
+
+    private Preference.OnPreferenceChangeListener mProximityListener =
+        new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if ((boolean) newValue) {
+                if (preference.getKey().equals(KEY_HAND_WAVE)) {
+                    mProximityWakePreference.setChecked(false);
+                } else if (preference.getKey().equals(KEY_PROXIMITY_WAKE)) {
+                    mHandwavePreference.setChecked(false);
+                }
+            }
+            return true;
+        }
+    };
+}
