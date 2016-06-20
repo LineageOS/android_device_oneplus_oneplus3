317940ff58b4bf16d348292a1c79e5331413b681
Kumar, Anand <anandkumar@codeaurora.org>
2016-06-20 19:03:06 +0530

    msm8996: Delete firmware file for msm8996
    Delete the dat and nv bin file for 8996,as these are not
    used for rome platform.
    
    CRs-Fixed: 1014722
    Change-Id: Ic8ba562da76bf772c86d0f9a04b1ea8098b0c552

--- device.mk
+++ device.mk
@@ -310,8 +310,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
