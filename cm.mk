7798c67d19cbcb8b757a0380f463fda263b4e4b9
dianlujitao <dianlujitao@gmail.com>
2016-06-25 00:09:56 +0800

    oneplus3: Move vendor proprietaries override to cm.mk
     * As per CM device did (e.g. bacon)
    
    Change-Id: Ie413669658c44fff47591df8e4bef19e1c31d7d8

--- cm.mk
+++ cm.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 PRODUCT_NAME := cm_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
@@ -26,5 +29,11 @@ PRODUCT_BRAND := OnePlus
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+TARGET_VENDOR_PRODUCT_NAME := OnePlus3
+TARGET_VENDOR_DEVICE_NAME := OnePlus3
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus3 PRODUCT_NAME=OnePlus3
+
+## Use the latest approved GMS identifiers unless running a signed build
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=OnePlus/OnePlus3/OnePlus3:6.0.1/MMB29M/213690:user/release-keys \
+    PRIVATE_BUILD_DESC="OnePlus3-user 6.0.1 MMB29M 42 dev-keys"
