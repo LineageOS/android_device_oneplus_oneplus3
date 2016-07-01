80e83332d083ae531cb341f651252802a7e78fda
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    op3: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- device.mk
+++ device.mk
@@ -174,7 +174,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.hw.aac.encoder=true \
     persist.vendor.bt.enable.splita2dp=false \
-    ro.config.media_vol_steps=25
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7
 
 # ANT+
 PRODUCT_PACKAGES += \
