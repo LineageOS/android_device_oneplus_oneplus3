d35ecfb571f3c9e5c9d459caea1830c75d6c7294
Steve Kondik <steve@cyngn.com>
2016-07-20 10:12:26 -0700

    op3: Fix GPS makefile
    Change-Id: I54da165dffd01cc115d1dc2ffce8023096506f6b

--- gps/loc_api/Android.mk
+++ gps/loc_api/Android.mk
@@ -10,7 +10,9 @@ endif #TARGET_NO_RPC
 GPS_DIR_LIST += $(LOCAL_PATH)/libloc_api_50001/
 
 ifneq ($(QCPATH),)
-GPS_DIR_LIST += ds_api loc_api_v02
+GPS_DIR_LIST += \
+    $(LOCAL_PATH)/ds_api/ \
+    $(LOCAL_PATH)/loc_api_v02/
 endif
 
 #call the subfolders
