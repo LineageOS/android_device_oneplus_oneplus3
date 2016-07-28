b92b38668176abada99c954598341d6278e73e91
Steve Kondik <steve@cyngn.com>
2016-07-28 05:06:26 -0700

    op3: Lights improvements
     * Set up the RGB sync stuff, add happy dance and rainbows too omg!
    
    Change-Id: I3517994c93ccc2d1789d7ceecf8b445ff3d27d5b

--- liblight/lights.c
+++ liblight/lights.c
@@ -17,7 +17,6 @@
  */
 
 #define LOG_TAG "lights"
-
 #include <cutils/log.h>
 
 #include <stdint.h>
@@ -110,6 +109,9 @@ char const*const GREEN_BLINK_FILE
 char const*const BLUE_BLINK_FILE
         = "/sys/class/leds/blue/blink";
 
+char const*const RGB_BLINK_FILE
+        = "/sys/class/leds/rgb/rgb_blink";
+
 #define RAMP_SIZE 8
 static int BRIGHTNESS_RAMP[RAMP_SIZE]
         = { 0, 12, 25, 37, 50, 72, 85, 100 };
@@ -118,12 +120,6 @@ static int BRIGHTNESS_RAMP[RAMP_SIZE]
 /**
  * device methods
  */
-
-void init_globals(void)
-{
-    // init the mutex
-    pthread_mutex_init(&g_lock, NULL);
-}
 
 static int
 write_int(char const* path, int value)
@@ -167,6 +163,12 @@ write_str(char const* path, char* value)
         }
         return -errno;
     }
+}
+
+void init_globals(void)
+{
+    // init the mutex
+    pthread_mutex_init(&g_lock, NULL);
 }
 
 static int
@@ -265,16 +267,10 @@ set_speaker_light_locked(struct light_device_t* dev,
     red = (colorRGB >> 16) & 0xFF;
     green = (colorRGB >> 8) & 0xFF;
     blue = colorRGB & 0xFF;
-    // bias for true white
-    if (colorRGB != 0 && red == green && green == blue) {
-        blue = (blue * 171) / 256;
-    }
     blink = onMS > 0 && offMS > 0;
 
     // disable all blinking to start
-    write_int(RED_BLINK_FILE, 0);
-    write_int(GREEN_BLINK_FILE, 0);
-    write_int(BLUE_BLINK_FILE, 0);
+    write_int(RGB_BLINK_FILE, 0);
 
     if (blink) {
         stepDuration = RAMP_STEP_DURATION;
@@ -318,11 +314,14 @@ set_speaker_light_locked(struct light_device_t* dev,
         free(duty);
 
         // start the party
-        write_int(RED_BLINK_FILE, red);
-        write_int(GREEN_BLINK_FILE, green);
-        write_int(BLUE_BLINK_FILE, blue);
+        write_int(RGB_BLINK_FILE, 1);
 
     } else {
+        if (red == 0 && green == 0 && blue == 0) {
+            write_int(RED_BLINK_FILE, 0);
+            write_int(GREEN_BLINK_FILE, 0);
+            write_int(BLUE_BLINK_FILE, 0);
+        }
         write_int(RED_LED_FILE, red);
         write_int(GREEN_LED_FILE, green);
         write_int(BLUE_LED_FILE, blue);
