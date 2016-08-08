506be1a7df2550d695d23eca048032c4639b6bf2
dianlujitao <dianlujitao@gmail.com>
2016-08-08 17:10:09 +0800

    op3: Remove healthd extension
     * Remove generic charger images, a new one is included in vendor/cm.
     * Remove libhealthd.op3, there's no need to keep it.
    
    Change-Id: I640c21d81173d39e41b05534104a55ab43804f82

--- healthd/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := healthd_board_msm.cpp
-LOCAL_MODULE := libhealthd.op3
-LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
-include $(BUILD_STATIC_LIBRARY)
