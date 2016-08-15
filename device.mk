3cac0adbb945c1039535de9dd223e1b1c0868183
Steve Kondik <steve@cyngn.com>
2016-08-14 22:05:28 -0700

    op3: Update for new display calibration
     * Map our new modes into LiveDisplay
        - Standard: White point corection @ 7200K
        - Dynamic: Vibrance enhanced
        - Photography: sRGB gamut mapping
        - Cinema: Gamma correction + white point @ 6500K
        - Astronomy: Very red for night vision preservation
        - Basic: Native display
     * Clean up / relocate a few things too
    
    Change-Id: I1d994737464a40f96fec9497eaad0f3d6525c735

--- device.mk
+++ device.mk
@@ -157,8 +157,9 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/FOSSConfig.xml:system/etc/FOSSConfig.xml
+PRODUCT_PACKAGES += \
+    FOSSConfig.xml \
+    qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml
 
 # Doze mode
 PRODUCT_PACKAGES += \
