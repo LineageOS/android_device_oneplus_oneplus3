84ab4133acf29f8a1fa53f8aec63be4776d071ca
padarshr <padarshr@codeaurora.org>
2016-08-19 21:50:51 +0530

    Add net_bind permissions to pm-service
    This change will add net_bind permissions to
    pm-service module
    
    Change-Id: I747e3c6d59c8c6255b1cc09750c9750c8e5a332b

--- android_filesystem_config.h
+++ android_filesystem_config.h
@@ -35,6 +35,7 @@ static const struct fs_path_config android_device_files[] = {
       // { 00755, AID_UID,     AID_GID,     (1ULL << CAPABILITY), "PATH_TO_BINARY" },
        { 00755, AID_BLUETOOTH,      AID_BLUETOOTH,      (1ULL << CAP_BLOCK_SUSPEND), "system/bin/wcnss_filter" },
        { 00755, AID_SYSTEM,      AID_SYSTEM,      (1ULL << CAP_NET_BIND_SERVICE), "system/bin/cnss-daemon"},
+       { 00755, AID_SYSTEM,      AID_SYSTEM,      (1ULL << CAP_NET_BIND_SERVICE), "system/bin/pm-service"},
        { 00755, AID_SYSTEM,         AID_SYSTEM,         (1ULL << CAP_NET_BIND_SERVICE), "system/bin/imsdatadaemon" },
        { 00755, AID_SYSTEM,         AID_RADIO,          (1ULL << CAP_NET_BIND_SERVICE), "system/bin/ims_rtp_daemon" },
 #ifdef NO_ANDROID_FILESYSTEM_CONFIG_DEVICE_DIRS
