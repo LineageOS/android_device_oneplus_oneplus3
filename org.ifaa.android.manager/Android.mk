535befffbcdba395612ba8ac69494ef1de55e89b
dianlujitao <dianlujitao@gmail.com>
2016-08-20 15:19:14 +0800

    op3: Support WeChat fingerprint payment
     * Credits to wuxianlin <wuxianlinwxl@gmail.com> for initial
       reverse engineering.
    
    Change-Id: I7d6e2ee7c27f771afd5c9370a9e2397e427f7610

--- org.ifaa.android.manager/Android.mk
+++ org.ifaa.android.manager/Android.mk
@@ -22,5 +22,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_MODULE := org.ifaa.android.manager
 LOCAL_MODULE_TAGS := optional
+LOCAL_JAVA_LIBRARIES := bouncycastle
 
 include $(BUILD_JAVA_LIBRARY)
