8286ca665a53294b074cf8733b778b95a42455ce
Steve Kondik <steve@cyngn.com>
2016-08-28 21:13:36 -0700

    op3: Remove bogus dependency
    Change-Id: Ic4d86561cc89916817cea989a323baebe72f0e74

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -16,7 +16,6 @@ LOCAL_C_INCLUDES += external/icu/icu4c/source/common
 else
 LOCAL_C_INCLUDES += external/icu4c/common
 endif
-LOCAL_C_INCLUDES += external/dhcpcd
 LOCAL_C_INCLUDES += external/libxml2/include
 LOCAL_C_INCLUDES += external/libnetfilter_conntrack/include
 LOCAL_C_INCLUDES += external/libnfnetlink/include
@@ -68,7 +67,6 @@ LOCAL_SHARED_LIBRARIES := libipanat
 LOCAL_SHARED_LIBRARIES += libxml2
 LOCAL_SHARED_LIBRARIES += libnfnetlink
 LOCAL_SHARED_LIBRARIES += libnetfilter_conntrack
-LOCAL_SHARED_LIBRARIES += libdhcpcd
 LOCAL_CLANG := true
 include $(BUILD_EXECUTABLE)
 
