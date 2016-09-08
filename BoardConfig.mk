bf92a5592e61b746ca8e1bf77c58c4082b8cfb74
Steve Kondik <steve@cyngn.com>
2016-09-08 13:55:35 -0700

    op3: Turn off SELinux for a bit
    Change-Id: I2cb22f7fa04bf7c58a55de1b9fcbc4daadd8b718

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,7 +57,7 @@ ENABLE_CPUSETS := true
 TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
