2597265ce88c7073a1d900303684b2516b256254
Steve Kondik <steve@cyngn.com>
2016-09-08 04:08:20 -0700

    op3: Update for new blobs
     * Remove pinning for now since there is churn
    
    Change-Id: If9d5385aa8db4ff33684bd84d4f1caf5907a8955

--- device.mk
+++ device.mk
@@ -312,5 +312,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Telephony extension
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
