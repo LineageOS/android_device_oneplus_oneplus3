77472596d1d2a8bf5ab5f219bfd1a0d2d3d03366
thecrazyskull <anaskarbila@aospa.co>
2016-09-29 15:16:40 -0400

    oneplus3: kang oneplus2 camera wrapper
    Change-Id: If8475118bdcc390808bbd2bb128ce4409fe0e1d9

--- camera/Android.mk
+++ camera/Android.mk
@@ -24,3 +24,18 @@ LOCAL_32_BIT_ONLY := true
 LOCAL_SHARED_LIBRARIES := libgui
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES := system/media/camera/include
+
+LOCAL_SRC_FILES := CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libgui libutils
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
