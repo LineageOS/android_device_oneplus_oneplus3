46bde30599e707d5a70893fccf643a8aeaf232f3
dianlujitao <dianlujitao@gmail.com>
2016-12-03 15:24:11 +0800

    op3: Stop preloading libNimsWrap.so
    Change-Id: I8af0fb691a44e383d47bc70c132b7bf9cdfe83f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
-TARGET_LDPRELOAD := libNimsWrap.so
 BOARD_USES_QCNE := true
 
 # Crypto
