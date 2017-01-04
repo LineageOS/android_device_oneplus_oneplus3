fb92fe98a52eedf3d237e288ddff4099b5c7eb80
flo071 <flo.hintermeier@gmail.com>
2017-01-04 20:09:40 +0100

    op3: update for LineageOS
    Change-Id: Ibbd4a242b42d6c0a577c6ac07192c241a9edf3b0

--- cm.mk
+++ cm.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/oneplus/oneplus3/device.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_oneplus3
+PRODUCT_NAME := lineage_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
