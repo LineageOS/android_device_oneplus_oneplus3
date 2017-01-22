600b04acbbf2ff9cf08e5a3509cf8936da0727dd
dianlujitao <dianlujitao@gmail.com>
2017-01-22 12:13:40 +0000

    Revert "op3: Make qcacld driver a module"
    This reverts commit 7bb7b226f7f7cfda2520f45fd5789d3390d8e380.
    
    Change-Id: I3e5c22d155d8c14ab697f4367d51708328e70fde

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,8 +224,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_MODULE_NAME := "wlan"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
