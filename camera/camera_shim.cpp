807cc591c2c0f42d855e8c9bfb4641570c131d70
dianlujitao <dianlujitao@gmail.com>
2017-01-22 23:06:12 +0800

    op3: Update to N camera blobs
     * Remove hax for HAL1, no longer needed
     * Disable HDR 1x frame for Snap, caused taking 2 pictures
       at a time in HDR mode
     * Keep using HAL1
     * Set persist.camera.HAL3.enabled after mounting /data
     * Clean up camera shim library
     * Stop using legacy open for cameras
    
    Change-Id: I9cf7574e9e8a79f76575edfc5680ba20a9550031

--- camera/camera_shim.cpp
+++ camera/camera_shim.cpp
@@ -14,20 +14,9 @@
  * limitations under the License.
  */
 
-#include <stdio.h>
 #include <string>
 
 const char *_ZN7android18gClientPackageNameE;
-
-extern "C" {
-
-extern ssize_t _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void*, void*);
-
-ssize_t _ZNK7android13SensorManager13getSensorListEPPKPKNS_6SensorE(void* thiz, void* list) {
-    return _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE(thiz, list);
-}
-
-}
 
 // GraphicBuffer(uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
 //               uint32_t inUsage, std::string requestorName = "<Unknown>");
