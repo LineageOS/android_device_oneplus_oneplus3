19a59d325bf0fc7bed69350de86c10d093095634
Vijay Venkatraman <vijaykv@google.com>
2017-02-13 18:37:37 -0800

    Replacing copy headers with header libraries
    Bug: 33241851
    Test: build sailfish target and pass cts telephony tests
    Change-Id: If84e7dfad12696c2e706101fd02c1c3d198de491

--- thermal-engine/Android.mk
+++ thermal-engine/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_COPY_HEADERS_TO := thermal-engine
-LOCAL_COPY_HEADERS := ./thermal_client.h
+LOCAL_MODULE:=thermal-engine
 
-include $(BUILD_COPY_HEADERS)
+LOCAL_EXPORT_C_INCLUDE_DIRS:=$(LOCAL_PATH)
+
+include $(BUILD_HEADER_LIBRARY)
