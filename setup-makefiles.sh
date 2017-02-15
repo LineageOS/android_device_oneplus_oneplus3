068251d2a466a60db890548944869383c3f53a33
Peter Nyilas <github@criminal.hu>
2017-02-15 23:34:25 +0000

    Revert "op3: Include aptX codec support"
    This reverts commit be592ef15f2dbc352ecd9b7ee03cbe5af63110ac.
    
    Change-Id: I37f700c2ce5c3e067b978e5fb9e3b65ecdbc2df2

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -77,7 +77,6 @@ endif
 
 EOF
 
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/bluetooth/bluetooth-vendor.mk)" >> "$PRODUCTMK"
 printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8996/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
 
 # We are done!
