7647cc7dd7d58ceacddaeb28885cf388fb5a848b
Vinay Verma <vvinay@codeaurora.org>
2017-02-17 17:33:34 +0530

    op3: Enable keymster HAL compilation
    Enable impl HAL compilation for keymaster.
    
    Change-Id: I65d0b85b0263ccb02d95bdc6532f5a1b8545fde4

--- device.mk
+++ device.mk
@@ -226,6 +226,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020.kl:system/usr/keylayout/fpc1020.kl \
     $(LOCAL_PATH)/keylayout/synaptics.kl:system/usr/keylayout/synaptics.kl
 
+# Keymaster Impl HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8996
