f0633b14ebc33b049a0228719942caee2ed5b5b6
flo071 <flo.hintermeier@gmail.com>
2017-03-18 12:22:01 +0100

    add high performance VR feature
    Change-Id: I1fe4957abb4aa99f2f35c3b3b45326c297374c10

--- device.mk
+++ device.mk
@@ -208,6 +208,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# High performance VR feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
