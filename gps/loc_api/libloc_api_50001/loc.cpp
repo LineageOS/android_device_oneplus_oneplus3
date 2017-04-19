f701b6034ea1cf11a259734861c55635dc939e05
Yingjie Wang <yingjiewang@codeaurora.org>
2017-04-19 17:19:16 +0800

    Re-enable delete_aiding_data for user build
    Allow delete aiding data request from HAL API for
    user build.
    
    Change-Id: Icfafe07b4217e482e4ca402b9a1da5ca70e1ff01
    CRs-fixed: 2034718

--- gps/loc_api/libloc_api_50001/loc.cpp
+++ gps/loc_api/libloc_api_50001/loc.cpp
@@ -567,9 +567,7 @@ static void loc_delete_aiding_data(GpsAidingData f)
 {
     ENTRY_LOG();
 
-#ifndef TARGET_BUILD_VARIANT_USER
     loc_eng_delete_aiding_data(loc_afw_data, f);
-#endif
 
     EXIT_LOG(%s, VOID_RET);
 }
