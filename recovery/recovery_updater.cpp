a65dc4f209eee346ffc11e40e7de5de97c099891
Michael Bestas <mkbestas@lineageos.org>
2017-04-28 22:34:12 +0300

    recovery: Use project pathmap for recovery
    Change-Id: Ifb7176454b5237ec366e5634e0188a6ce24740dd

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "edify/expr.h"
@@ -34,13 +35,13 @@
 #define ALPHABET_LEN 256
 
 #ifdef USES_BOOTDEVICE_PATH
-#define TZ_PART_PATH "/dev/block/bootdevice/by-name/tz"
+#define MODEM_PART_PATH "/dev/block/bootdevice/by-name/modem"
 #else
-#define TZ_PART_PATH "/dev/block/platform/msm_sdcc.1/by-name/tz"
+#define MODEM_PART_PATH "/dev/block/platform/msm_sdcc.1/by-name/modem"
 #endif
-#define TZ_VER_STR "QC_IMAGE_VERSION_STRING="
-#define TZ_VER_STR_LEN 24
-#define TZ_VER_BUF_LEN 255
+#define MODEM_VER_STR "Time_Stamp\": \""
+#define MODEM_VER_STR_LEN 14
+#define MODEM_VER_BUF_LEN 20
 
 /* Boyer-Moore string search implementation from Wikipedia */
 
@@ -120,80 +121,81 @@ static char * bm_search(const char *str, size_t str_len, const char *pat,
     return NULL;
 }
 
-static int get_tz_version(char *ver_str, size_t len) {
+static int get_modem_version(char *ver_str, size_t len) {
     int ret = 0;
     int fd;
-    int tz_size;
-    char *tz_data = NULL;
+    int modem_size;
+    char *modem_data = NULL;
     char *offset = NULL;
 
-    fd = open(TZ_PART_PATH, O_RDONLY);
+    fd = open(MODEM_PART_PATH, O_RDONLY);
     if (fd < 0) {
         ret = errno;
         goto err_ret;
     }
 
-    tz_size = lseek64(fd, 0, SEEK_END);
-    if (tz_size == -1) {
+    modem_size = lseek64(fd, 0, SEEK_END);
+    if (modem_size == -1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    tz_data = (char *) mmap(NULL, tz_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (tz_data == (char *)-1) {
+    modem_data = (char *) mmap(NULL, modem_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (modem_data == (char *)-1) {
         ret = errno;
         goto err_fd_close;
     }
 
-    /* Do Boyer-Moore search across TZ data */
-    offset = bm_search(tz_data, tz_size, TZ_VER_STR, TZ_VER_STR_LEN);
+    /* Do Boyer-Moore search across MODEM data */
+    offset = bm_search(modem_data, modem_size, MODEM_VER_STR, MODEM_VER_STR_LEN);
     if (offset != NULL) {
-        strncpy(ver_str, offset + TZ_VER_STR_LEN, len);
+        snprintf(ver_str, len, "%s", offset + MODEM_VER_STR_LEN);
     } else {
         ret = -ENOENT;
     }
 
-    munmap(tz_data, tz_size);
+    munmap(modem_data, modem_size);
 err_fd_close:
     close(fd);
 err_ret:
     return ret;
 }
 
-/* verify_trustzone("TZ_VERSION", "TZ_VERSION", ...) */
-Value * VerifyTrustZoneFn(const char *name, State *state, int argc, Expr *argv[]) {
-    char current_tz_version[TZ_VER_BUF_LEN];
-    int i, ret;
+/* verify_modem("MODEM_VERSION") */
+Value * VerifyModemFn(const char *name, State *state, int argc, Expr *argv[]) {
+    char current_modem_version[MODEM_VER_BUF_LEN];
+    char* modem_version;
+    int ret;
+    struct tm tm1, tm2;
 
-    ret = get_tz_version(current_tz_version, TZ_VER_BUF_LEN);
+    ret = get_modem_version(current_modem_version, MODEM_VER_BUF_LEN);
     if (ret) {
-        return ErrorAbort(state, kFreadFailure, "%s() failed to read current TZ version: %d",
-                name, ret);
+        return ErrorAbort(state, kVendorFailure,
+                "%s() failed to read current MODEM build time-stamp: %d", name, ret);
     }
 
-    char** tz_version = ReadVarArgs(state, argc, argv);
-    if (tz_version == NULL) {
+    memset(&tm1, 0, sizeof(tm));
+    strptime(current_modem_version, "%Y-%m-%d %H:%M:%S", &tm1);
+
+    ret = ReadArgs(state, argv, 1, &modem_version);
+    if (ret < 0) {
         return ErrorAbort(state, kArgsParsingFailure, "%s() error parsing arguments", name);
     }
 
-    ret = 0;
-    for (i = 0; i < argc; i++) {
-        uiPrintf(state, "Comparing TZ version %s to %s",
-                tz_version[i], current_tz_version);
-        if (strncmp(tz_version[i], current_tz_version, strlen(tz_version[i])) == 0) {
-            ret = 1;
-            break;
-        }
+    uiPrintf(state, "Checking for MODEM build time-stamp %s\n", modem_version);
+
+    memset(&tm2, 0, sizeof(tm));
+    strptime(modem_version, "%Y-%m-%d %H:%M:%S", &tm2);
+
+    if (mktime(&tm1) >= mktime(&tm2)) {
+        ret = 1;
     }
 
-    for (i = 0; i < argc; i++) {
-        free(tz_version[i]);
-    }
-    free(tz_version);
+    free(modem_version);
 
     return StringValue(strdup(ret ? "1" : "0"));
 }
 
 void Register_librecovery_updater_op3() {
-    RegisterFunction("op3.verify_trustzone", VerifyTrustZoneFn);
+    RegisterFunction("op3.verify_modem", VerifyModemFn);
 }
