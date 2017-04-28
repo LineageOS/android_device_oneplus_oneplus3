ea2917a9bde4eb4da96041f116695887015f0408
Michael Bestas <mkbestas@lineageos.org>
2017-04-28 22:34:12 +0300

    recovery: Use project pathmap for recovery
    Change-Id: Ifb7176454b5237ec366e5634e0188a6ce24740dd

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -6,7 +6,7 @@ ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
 LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_op3
 LOCAL_MODULE_TAGS := eng
