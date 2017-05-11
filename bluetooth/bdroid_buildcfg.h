06ac59f07e0732e0d7636cf83160d0c1dd5a68f9
Umesh Vats <uvats@codeaurora.org>
2017-05-11 12:03:55 -0700

    op3: Bluetooth: Increase number of SEPs to support SHO/Mcast
    Icreased number of SEPs to support SHO/Mcast
    
    Change-Id: I3cfb4e8d4d6969e442406bd095a865d49947151d

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -52,4 +52,7 @@ static inline const char* BtmGetDefaultName()
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
+
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
