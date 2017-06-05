8672aa239c655ee417a884caaee3127c5bd204ae
Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>
2017-06-05 12:05:08 +0530

    op3: Add android.hardware.drm@1.0-impl to PRODUCT_PACKAGES
    Add android.hardware.drm@1.0-impl.so library to PRODUCT_PACKAGES.
    
    Change-Id: I28aa4861256579a4e8d06709a10d0d6711c0cd65
    Signed-off-by: Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OneplusDoze
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     fingerprintd \
