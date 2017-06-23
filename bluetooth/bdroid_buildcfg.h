9a25a6a15f369b194851ceb0d30de5a8c34d96c0
dianlujitao <dianlujitao@lineageos.org>
2017-06-23 09:35:20 +0800

    op3: Sync bluetooth configuration with CAF
    Change-Id: I00e83f5cecf01171cf2f309f02c487e608a4d8be

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,13 +22,11 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "OnePlus 3"
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
+#define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
