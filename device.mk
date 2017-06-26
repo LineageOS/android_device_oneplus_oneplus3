6a0b62ef50c968cd60f3b8c6563b38f9ffd449a6
Kevin Rocard <krocard@google.com>
2017-06-26 15:17:03 -0700

    audio: Convert audio_effects.conf to audio_effects.xml
    Converted .conf format to .xml using:
    https://github.com/luk1337/aeffects-conf2xml
    
    Bug: 37492580
    Test: dumped the effectFactory state and check it was unchanged
          $ adb shell dumpsys media.audio_flinger | sed '/Clients:/q'
    Change-Id: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Merged-In: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/audio/etc/audio_policy_configuration.xml \
