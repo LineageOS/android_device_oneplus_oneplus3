dd01451739d00805faf896e037d84138ce103413
Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>
2017-06-30 18:05:31 +0530

    op3: Add android.hardware.drm@1.0-service to PRODUCT_PACKAGES
    Add android.hardware.drm@1.0-service to PRODUCT_PACKAGES.
    
    Change-Id: I58221d1deae285f48389c139d6edffb0e730f737
    Signed-off-by: Udaya Bhaskara Reddy Mallavarapu <udaym@codeaurora.org>

--- device.mk
+++ device.mk
@@ -208,7 +208,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
