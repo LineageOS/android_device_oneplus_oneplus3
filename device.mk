66883354295798e7f936331a572cad5ada7d6091
padarshr <padarshr@codeaurora.org>
2017-07-12 16:46:43 +0530

    op3: Update vndk-sp build mechanism and library set.
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.
    
    Change-Id: Iac86c8ece08c2dae94fce6e94deb670aa742e3dc

--- device.mk
+++ device.mk
@@ -394,25 +394,9 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# Vndk-sp
+# VNDK-SP:
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0.vndk-sp\
-    android.hardware.graphics.allocator@2.0.vndk-sp\
-    android.hardware.graphics.mapper@2.0.vndk-sp\
-    android.hardware.graphics.common@1.0.vndk-sp\
-    android.hidl.base@1.0.vndk-sp\
-    libhwbinder.vndk-sp\
-    libbase.vndk-sp\
-    libcutils.vndk-sp\
-    libhardware.vndk-sp\
-    libhidlbase.vndk-sp\
-    libhidltransport.vndk-sp\
-    libutils.vndk-sp\
-    libc++.vndk-sp\
-    libsync.vndk-sp\
-    libbacktrace.vndk-sp\
-    libunwind.vndk-sp\
-    liblzma.vndk-sp\
+    vndk-sp
 
 # VR
 PRODUCT_PACKAGES += \
