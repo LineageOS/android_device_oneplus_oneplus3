a850fc0f03583a61d9b307cca91565528a17c3b2
Luca Stefani <luca020400@lineageos.org>
2017-07-18 18:24:43 +0200

    op3: data-ipa-cfg-mgr: Disable DEBUG for userdebug builds
    Change-Id: Idaee3e6d5b3759028918559b70e04547809bc09b

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -29,7 +29,7 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_CFLAGS := -DFEATURE_IPA_ANDROID
 LOCAL_CFLAGS += -DFEATURE_IPACM_HAL -Wall -Werror -Wno-error=macro-redefined
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DDEBUG
 endif
 
