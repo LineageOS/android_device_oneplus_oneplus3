4fe0ea5ad648a9c10c9ca305f9f3a8f60dbdb181
Sultanxda <sultanxda@gmail.com>
2017-07-31 10:42:47 -0700

    op3: Automatically sed camera HAL symbol changes needed for shim
    No need to pin the camera HAL blob anymore.
    
    Change-Id: I3d3ec1b8121739548ca6b36add549b648fb04fcc

--- extract-files.sh
+++ extract-files.sh
@@ -57,3 +57,11 @@ extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC"
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh
+
+CAMERA_HAL="$CM_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/lib/hw/camera.msm8996.so
+
+sed -i \
+    -e 's/_ZN7qcamera17QCameraParameters16setQuadraCfaModeEjb/_ZN7qcamera17QCameraParameters16setQuadraCfaModSHIM/' \
+    -e 's/_ZN7qcamera17QCameraParameters12setQuadraCfaERKS0_/_ZN7qcamera17QCameraParameters12setQuadraCfaERSHIM/' \
+    -e 's/_ZN7qcamera17QCameraParameters12getQuadraCfaEv/_ZN7qcamera17QCameraParameters12getQuadraCSHIM/' \
+    "$CAMERA_HAL"
