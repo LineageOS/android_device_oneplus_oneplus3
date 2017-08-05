7c75553a4d37a33d02868526eed608b70ce6f992
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2017-08-06 00:51:40 +0530

    [CNE]: start cnd daemon from vendor
    start cnd daemon from vendor with capabilities
    net_bind_service, block_suspend.
    
    Change-Id: I53f5d72b5db39d9bd66620fbcea3ae442534e9d1

--- android_filesystem_config.h
+++ android_filesystem_config.h
@@ -42,6 +42,14 @@ static const struct fs_path_config android_device_files[] = {
        { 00755, AID_SYSTEM,      AID_SYSTEM,      (1ULL << CAP_NET_BIND_SERVICE), "vendor/bin/imsdatadaemon" },
        { 00755, AID_GPS,         AID_GPS,         (1ULL << CAP_NET_BIND_SERVICE), "system/vendor/bin/slim_daemon" },
        { 00755, AID_GPS,         AID_GPS,         (1ULL << CAP_NET_BIND_SERVICE), "vendor/vendor/bin/slim_daemon" },
+       { 00755, AID_SYSTEM,      AID_SYSTEM,      CAP_MASK_LONG(CAP_NET_BIND_SERVICE) |
+                                                  CAP_MASK_LONG(CAP_BLOCK_SUSPEND) |
+                                                  CAP_MASK_LONG(CAP_NET_ADMIN),
+                                                  "vendor/bin/cnd" },
+       { 00755, AID_SYSTEM,      AID_SYSTEM,      CAP_MASK_LONG(CAP_NET_BIND_SERVICE) |
+                                                  CAP_MASK_LONG(CAP_BLOCK_SUSPEND) |
+                                                  CAP_MASK_LONG(CAP_NET_ADMIN),
+                                                  "system/vendor/bin/cnd" },
        { 00755, AID_SYSTEM,      AID_RADIO,       (1ULL << CAP_NET_BIND_SERVICE), "system/vendor/bin/ims_rtp_daemon" },
        { 00755, AID_SYSTEM,      AID_RADIO,       (1ULL << CAP_NET_BIND_SERVICE), "vendor/bin/ims_rtp_daemon" },
 #ifdef NO_ANDROID_FILESYSTEM_CONFIG_DEVICE_DIRS
