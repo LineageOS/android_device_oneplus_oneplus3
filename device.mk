89f038c31296cd2f9dc744d57fc57370681f6794
Mahantesh Sanakall <msanak@codeaurora.org>
2017-08-07 16:32:26 +0530

    op3: Rename the device manifest file.
    Rename the device manifest(vintf.xml) to manifest.xml and move
    away from building device manifest through PRODUCT_COPY_FILES.
    (It will be built through another change using the build
    variable - DEVICE_MANIFEST_FILE).
    
    Change-Id: I89144c12e634c9d7d4f2928e5ada1675ea7d95e4

--- device.mk
+++ device.mk
@@ -233,7 +233,7 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vintf.xml:system/vendor/manifest.xml
+    $(LOCAL_PATH)/configs/manifest.xml:system/vendor/manifest.xml
 
 # IPv6
 PRODUCT_PACKAGES += \
