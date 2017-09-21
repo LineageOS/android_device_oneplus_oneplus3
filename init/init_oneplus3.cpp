9d289e566d47c1971d0f0061746ae9cf3c7a6ea7
dianlujitao <dianlujitao@lineageos.org>
2017-09-21 22:17:02 +0800

    op3: Update blobs from OB24 / OB15
    Change-Id: Ic117e8454f6ccce941acd71c363113b0b819b856

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -106,16 +106,16 @@ void load_op3(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3");
-    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 85 dev-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/08161000:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 95 dev-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/09151000:user/release-keys");
 }
 
 void load_op3t(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3T");
-    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 71 dev-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/08161130:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 83 dev-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/09151130:user/release-keys");
     property_set("ro.power_profile.override", "power_profile_3t");
 }
 
