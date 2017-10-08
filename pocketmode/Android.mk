7d08b064582b2ca398551ed986d124c2dcee174c
Bruno Martins <bgcngm@gmail.com>
2017-10-08 10:12:51 +0100

    op3: PocketMode: Apply LineageOS rebrand
    Change-Id: I1de627062f56bd125f430033e8bb7aad8fe48f69

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -10,7 +10,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.cyanogenmod.platform
+    org.lineageos.platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
--- pocketmode/src/org/lineageos/pocketmode/BootCompletedReceiver.java
+++ pocketmode/src/org/lineageos/pocketmode/BootCompletedReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.pocketmode;
+package org.lineageos.pocketmode;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
