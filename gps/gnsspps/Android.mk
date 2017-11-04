c36048214864baff2966126fdb2e20cc21f4f0cf
dianlujitao <dianlujitao@lineageos.org>
2017-11-04 14:23:21 +0800

    op3: Update libgnsspps makefile from LA.UM.6.5.r1-04000-8x96.0
    Change-Id: I5cd29c8b8a8b32d77795443808d92a7739608c73

--- gps/gnsspps/Android.mk
+++ gps/gnsspps/Android.mk
@@ -1,9 +1,11 @@
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libgnsspps
-
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := \
@@ -19,14 +21,16 @@ LOCAL_CFLAGS += \
     -fno-short-enums \
     -D_ANDROID_
 
-LOCAL_COPY_HEADERS_TO:= libgnsspps/
-
-LOCAL_COPY_HEADERS:= \
-    gnsspps.h
-
 ## Includes
-LOCAL_C_INCLUDES := \
-    $(TARGET_OUT_HEADERS)/gps.utils \
-    $(TARGET_OUT_HEADERS)/libloc_pla
-
+LOCAL_HEADER_LIBRARIES := \
+    libgps.utils_headers \
+    libloc_pla_headers
+LOCAL_CFLAGS += $(GNSS_CFLAGS)
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgnsspps_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HEADER_LIBRARY)
+
+endif
