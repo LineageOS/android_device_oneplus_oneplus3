5caa050e735b507fc04b3fd82310a02ab417f0f5
Yuwen <294958213@qq.com>
2017-11-06 02:58:46 +0800

    op3: Add WeChat fingerprint payment support
    Change-Id: I6ad08b03e60d3031223fe190c366946d85a57ac1

--- device.mk
+++ device.mk
@@ -103,12 +103,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# Alipay
+# Alipay & WeChat fingerprint payment
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
+    org.ifaa.android.manager \
+    soter
 
 PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    org.ifaa.android.manager \
+    soter
 
 # Audio
 PRODUCT_PACKAGES += \
