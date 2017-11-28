a62c6d0eebb9bae04ecc9f71d8099630815e64c3
dianlujitao <dianlujitao@lineageos.org>
2017-11-28 17:21:58 +0800

    op3: Update ifaa manager for v2 api
    Change-Id: If533f2eccafd9d0e10f421fdfc378a6f83832da8

---
+++ org.ifaa.android.manager/src/java/org/ifaa/android/manager/IFAAManagerV2.java
@@ -0,0 +1,7 @@
+package org.ifaa.android.manager;
+
+import android.content.Context;
+
+public abstract class IFAAManagerV2 extends IFAAManager{
+  public abstract byte[] processCmdV2(Context paramContext, byte[] paramArrayOfByte);
+}
