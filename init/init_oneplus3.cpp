b0b1e885e017207a60d81549ff9f4e69e69b8941
dianlujitao <dianlujitao@lineageos.org>
2017-11-28 17:11:43 +0800

    op3: Update build fingerprint from OxygenOS 5.0
    Change-Id: Ib678d5e2ae769cffaba9759a66d5ea3c29c0163a

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -106,16 +106,16 @@ void load_op3(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3");
-    property_override("ro.build.description", "OnePlus3-user 8.0.0 OPR6.170623.013 27 release-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR6.170623.013/11111505:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 8.0.0 OPR6.170623.013 77 release-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR6.170623.013/10250816:user/release-keys");
 }
 
 void load_op3t(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3T");
-    property_override("ro.build.description", "OnePlus3-user 8.0.0 OPR6.170623.013 29 release-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR6.170623.013/11111505:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 8.0.0 OPR6.170623.013 83 release-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR6.170623.013/10250816:user/release-keys");
     property_set("ro.power_profile.override", "power_profile_3t");
 }
 
