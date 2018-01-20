2ef527b862c2b3b6ddd7f3052d89d3b008138e93
dianlujitao <dianlujitao@lineageos.org>
2018-01-20 10:58:36 +0800

    op3: rootdir: Squashed update to LA.UM.6.5.r1-05300-8x96.0
    Change-Id: I7b37cf6710324b244f0851ee6757ac68563dc958

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -89,6 +89,20 @@ start_msm_irqbalance_8939()
 	fi
 }
 
+start_msm_irqbalance_8952()
+{
+        if [ -f /system/vendor/bin/msm_irqbalance ]; then
+                case "$platformid" in
+                     "241" | "263" | "264" | "268" | "269" | "270" | "271")
+                        start vendor.msm_irqbalance;;
+                esac
+                case "$platformid" in
+                     "266" | "274" | "277" | "278")
+                        start vendor.msm_irqbal_lb;;
+                esac
+	fi
+}
+
 start_msm_irqbalance660()
 {
 	if [ -f /vendor/bin/msm_irqbalance ]; then
@@ -292,6 +306,60 @@ case "$target" in
     "msm8909")
         start_vm_bms
         ;;
+    "msm8952")
+        start_msm_irqbalance_8952
+	 if [ -f /sys/devices/soc0/soc_id ]; then
+             soc_id=`cat /sys/devices/soc0/soc_id`
+         else
+             soc_id=`cat /sys/devices/system/soc/soc0/id`
+         fi
+
+	 if [ -f /sys/devices/soc0/platform_subtype_id ]; then
+	      platform_subtype_id=`cat /sys/devices/soc0/platform_subtype_id`
+	 fi
+	 if [ -f /sys/devices/soc0/hw_platform ]; then
+	       hw_platform=`cat /sys/devices/soc0/hw_platform`
+	 fi
+	 case "$soc_id" in
+	      "264")
+	           case "$hw_platform" in
+			    "Surf")
+			         case "$platform_subtype_id" in
+			              "1" | "2")
+			                  setprop qemu.hw.mainkeys 0
+			                  ;;
+				  esac
+			          ;;
+			    "MTP")
+			         case "$platform_subtype_id" in
+			              "3")
+			                  setprop qemu.hw.mainkeys 0
+			                  ;;
+				  esac
+			          ;;
+			    "QRD")
+			         case "$platform_subtype_id" in
+			              "0")
+			                  setprop qemu.hw.mainkeys 0
+			                  ;;
+				  esac
+				  ;;
+		     esac
+		     ;;
+		 "266" | "274" | "277" | "278")
+	              case "$hw_platform" in
+			       "Surf" | "RCM")
+                                    if [ $panel_xres -eq 1440 ]; then
+				       setprop qemu.hw.mainkeys 0
+				    fi
+				    ;;
+				"MTP" | "QRD")
+				       setprop qemu.hw.mainkeys 0
+				       ;;
+		      esac
+		      ;;
+	esac
+	;;
     "msm8937")
         start_msm_irqbalance_8939
         if [ -f /sys/devices/soc0/soc_id ]; then
