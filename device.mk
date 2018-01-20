15eaaf8d3c4d79604e306f50fa77ec5c8dee32cc
dianlujitao <dianlujitao@lineageos.org>
2018-01-20 11:27:15 +0800

    op3: Remove msm_irqbalance
     * Not used on msm8996.
    
    Change-Id: Icc8f7e438c794d018b5101ce5083332aaaa79f6a

--- device.mk
+++ device.mk
@@ -294,10 +294,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
