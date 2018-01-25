754d6fb1ec77a89d80aac635380a09e5ee7df646
dianlujitao <dianlujitao@lineageos.org>
2018-01-25 11:24:45 +0800

    op3: Switch to lineage vibrator HAL
    Change-Id: I01acea2bae938a105081c278f70795559e0f9526

--- device.mk
+++ device.mk
@@ -446,8 +446,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \
