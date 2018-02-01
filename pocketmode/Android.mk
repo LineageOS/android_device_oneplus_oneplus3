7883077f5df4cf07de3f43ff1b6dd59b0ccb5fbd
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-01 10:17:24 -0600

    op3: PocketMode: Explicitly include Android support libs
     * Fixes non-jack build
    
    Change-Id: I9d8346193577c7be218e12efb20f52ca9946eb14

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -9,6 +9,13 @@ LOCAL_PACKAGE_NAME := OneplusPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference
+
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform
 
