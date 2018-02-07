ee57440a05a4be995b308edfe2f711a4612f8b04
dianlujitao <dianlujitao@lineageos.org>
2018-02-07 12:07:27 +0800

    op3: Fork camera provider HAL
    Change-Id: I1da42d8aa2f9ad42da7d1ee6b7b69bf30c2873d1

--- device.mk
+++ device.mk
@@ -200,7 +200,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service.oneplus3 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
