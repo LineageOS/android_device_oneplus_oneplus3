724bea573ba56b2ede31b283277585bd496bba55
dianlujitao <dianlujitao@lineageos.org>
2018-02-09 12:51:18 +0800

    op3: Add initial Oreo sepolicy
    Change-Id: I9aa29ac101d662c56eafd0fa0a81301ac1bcd16a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,7 +220,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Treble
 #PRODUCT_FULL_TREBLE_OVERRIDE := true
