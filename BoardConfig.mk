e3f35252e199d7ebeb163f5f284a276edf3197b6
Paul Keith <javelinanddart@gmail.com>
2018-02-15 23:33:28 +0100

    op3: Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
    Change-Id: I877192422062f3e59c81a7130ad1a2b4be5d1647

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,6 +222,9 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
+# Shims
+TARGET_LD_SHIM_LIBS := /system/vendor/lib/hw/camera.msm8996.so|libcamera_shim.so:/system/lib/libcamera_client.so|libcamera_parameters_shim.so
+
 # Treble
 #PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
