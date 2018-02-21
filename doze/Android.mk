2c6b5858f9c7d48561d4e82e35e5db3892c910e6
Anton Hansson <hansson@google.com>
2018-02-21 14:11:44 +0000

    op3: Set LOCAL_SDK_VERSION where possible.
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant packages
    Bug: 73535841
    Change-Id: Ic906adbd670c4dbfe2cdf0b71cdbeaf9ac08a27e

--- doze/Android.mk
+++ doze/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := OneplusDoze
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_USE_AAPT2 := true
--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := OneplusPocketMode
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
