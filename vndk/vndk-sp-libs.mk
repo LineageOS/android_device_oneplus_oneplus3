6c93e8eaa43b96a23f4470f09b3d5aaedf290cee
Naseer Ahmed <naseer@codeaurora.org>
2018-03-12 18:33:53 -0400

    op3: Add Mapper 2.1 to VNDK SP list
    Change-Id: I24cbc517877e4e7f5827556061dcb5874918fc35

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,7 +1,9 @@
 VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
+    android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.common@1.0 \
+    android.hardware.graphics.common@1.1 \
     android.hardware.renderscript@1.0 \
     android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
