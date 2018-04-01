accdeac5812705ed3d135ee31305b6b83f5712dd
Bruno Martins <bgcngm@gmail.com>
2018-04-01 10:56:30 +0100

    op3: init: Handle overriding system and vendor props
     * Up until now, vendor build properties were added into /system/build.prop
       when property split isn't enabled. However, Google realised that
       either /vendor/build.prop or /system/vendor/build.prop can always exist,
       and decided to clean up code and always install vendor build props
       into $(TARGET_OUT_VENDOR)/build.prop.
    
     * For this reason, the unified devices that used to override build props
       such as build fingerprint, will need to override the matching vendor
       build props.
    
    Change-Id: Iacdd8eb67543daff5a46b92dbaf17cd094ce462b

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -52,6 +52,12 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 static int read_file2(const char *fname, char *data, int max_size)
 {
     int fd, rc;
