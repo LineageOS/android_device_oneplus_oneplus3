d687d331c4a1b13baa2a9aa4c0478f5092c66b52
dianlujitao <dianlujitao@lineageos.org>
2018-06-14 15:12:40 +0800

    op3: Move shim libs to system
    Change-Id: Ic5e30243935df6ee2eeb972cb9d438850b077ac8

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ LOCAL_MODULE := libcamera_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_32_BIT_ONLY := true
-LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -35,6 +34,5 @@ LOCAL_MODULE := libcamera_parameters_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
-LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
