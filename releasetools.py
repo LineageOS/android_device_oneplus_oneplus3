fcae26c8900f8a56bd50a9dd603693b7cb0486db
Tim Schumacher <timschumi@gmx.de>
2018-06-23 10:08:59 +0200

    oneplus3: releasetools: Split up getting android-info.txt
    input_zip does not exist when generating an incremental
    OTA, which results in an error when it tries to package
    one. The correct replacement for that would be target_zip.
    
    Split up the process of getting the device-specific
    information by passing android_info as a parameter to
    AddModemAssertion()
    
    Change-Id: Ib246658f71d9f30a0aa34e902a400ef108a3ed1f

--- releasetools.py
+++ releasetools.py
@@ -19,15 +19,16 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddModemAssertion(info)
+  android_info = info.input_zip.read("OTA/android-info.txt")
+  AddModemAssertion(info, android_info)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddModemAssertion(info)
+  android_info = info.target_zip.read("OTA/android-info.txt")
+  AddModemAssertion(info, android_info)
   return
 
-def AddModemAssertion(info):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+def AddModemAssertion(info, android_info):
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   if m:
     version = m.group(1).rstrip()
