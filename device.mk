3b69dc65f6e6a969fe704828c1dff4ef42919331
Abhishek Srivastava <cabsriv@codeaurora.org>
2018-07-16 14:48:45 +0530

    op3: wifi: Add move_wifi_data.sh
    Add move_wifi_data.sh for OTA update.
    
    Change-Id: I3b8c050878b0df3cc1cfcba27b7716ab0a9cde31
    CRs-Fixed: 2286278

--- device.mk
+++ device.mk
@@ -499,6 +499,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/move_wifi_data.sh:system/bin/move_wifi_data.sh \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
