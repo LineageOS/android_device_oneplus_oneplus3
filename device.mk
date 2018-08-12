6c16be356ebd9b8874e44164feec937bf5b1fa37
Ivan Grinko <iivanich@gmail.com>
2018-08-12 12:29:41 +0300

    op3: Create media_profiles_V1_0.xml
    * Delete media_profiles.xml
    * media_profiles_V1_0.xml is created from media_profiles.xml by replacing
      the dtd schema inside media_profiles.xml with an external reference to
      /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: I5bfbfa4ca8a1e4d2449c4257cb28560f149e13bd

--- device.mk
+++ device.mk
@@ -324,9 +324,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
-    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
