a0b1277342714e0cd50e288811e7c1683233949f
LuK1337 <priv.luk@gmail.com>
2018-09-07 15:21:20 +0200

    op3: Drop libbt-vendor
     * With QTI bluetooth it's no longer needed.
    
    Change-Id: Iba4b0b6ba24fe1b85e7175291ed84769615ffadd

--- device.mk
+++ device.mk
@@ -192,9 +192,6 @@ PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Bluetooth
-PRODUCT_PACKAGES += \
-    libbt-vendor
-
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=rome
 
