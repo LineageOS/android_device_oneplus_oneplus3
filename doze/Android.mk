60df74368afa9eab873dd1a1d9aeaa0e23ee75f1
Bruno Martins <bgcngm@gmail.com>
2018-09-16 00:04:07 +0100

    op3: Doze: Drop useless SDK dependency
    Change-Id: I74901339740b82b79f36754c7e52894a98020c3d

--- doze/Android.mk
+++ doze/Android.mk
@@ -20,9 +20,6 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v14-preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
