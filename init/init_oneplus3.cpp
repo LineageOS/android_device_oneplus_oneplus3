032865d7bcc914ab764fc565950d8b81d8fbec4e
dianlujitao <dianlujitao@lineageos.org>
2018-10-15 14:46:47 +0800

    op3: init: Check if power off alarm is set
    Check if power off alarm is set, then set alarm's properties
    
    Adapted from Mao Jinlong <c_jmao@codeaurora.org> patch in init.qcom.early_boot.sh
    
    Change-Id: Ib86584d1c09aba07c47435ac8bed39aac79a80f7

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -67,10 +67,13 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
 void init_alarm_boot_properties()
 {
     char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
-    std::string reboot_reason = property_get("ro.boot.alarmboot");
+    std::string power_off_alarm;
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
 
-    if (ReadFileToString(boot_reason_file, &boot_reason)) {
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
         /*
         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
         * For existing PMIC chips, the following mapping applies
@@ -86,7 +89,8 @@ void init_alarm_boot_properties()
         * 7 -> CBLPWR_N pin toggled (for external power supply)
         * 8 -> KPDPWR_N pin toggled (power key pressed)
         */
-        if (Trim(boot_reason) == "3" || reboot_reason == "true") {
+        if ((Trim(boot_reason) == "3" || reboot_reason == "true")
+                && Trim(power_off_alarm) == "1") {
             property_set("ro.alarm_boot", "true");
         } else {
             property_set("ro.alarm_boot", "false");
