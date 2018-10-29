8d8de4c652e8a18c1601ca7edb47cff559f1f1b6
dianlujitao <dianlujitao@lineageos.org>
2018-10-29 20:38:20 +0800

    op3: Update system properties from LA.UM.7.5.r1-03100-8x96.0
    Change-Id: Ib2684067ececef3527d386daae65708117eb4518

--- device.mk
+++ device.mk
@@ -91,12 +91,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     sys.vendor.shutdown.waittime=500 \
-    ro.build.shutdown_timeout=0 \
     ro.frp.pst=/dev/block/bootdevice/by-name/config \
     persist.radio.multisim.config=dsds
 
@@ -193,7 +192,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.bluetooth.soc=rome
+    vendor.qcom.bluetooth.soc=rome
 
 # Camera
 PRODUCT_PACKAGES += \
