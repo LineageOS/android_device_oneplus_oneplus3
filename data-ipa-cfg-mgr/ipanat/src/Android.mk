9af33eb24585a136f94ccc2fb0b4c2c918199991
Bruno Martins <bgcngm@gmail.com>
2018-10-29 13:48:35 +0000

    op3: data-ipa-cfg-mgr: Use generated kernel headers
    Change-Id: Ie33f0b8b7996536a73f660cba5b4700964d8b03f

--- data-ipa-cfg-mgr/ipanat/src/Android.mk
+++ data-ipa-cfg-mgr/ipanat/src/Android.mk
@@ -10,8 +10,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../inc
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_SRC_FILES := ipa_nat_drv.c \
                    ipa_nat_drvi.c
