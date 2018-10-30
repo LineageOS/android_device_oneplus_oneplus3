813e3e5cd5ab3bff0da576f12b1de571356ec340
dianlujitao <dianlujitao@lineageos.org>
2018-10-30 19:52:43 +0800

    op3: Update config.fs from LA.UM.7.5.r1-03100-8x96.0
    Change-Id: Iacaaead27275e4a8bb0abddd0a4dadc790a13839

--- config.fs
+++ config.fs
@@ -1,11 +1,20 @@
-[AID_QTI_DIAG]
+[AID_VENDOR_QTI_DIAG]
 value:2901
 
-[AID_RFS]
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
 value:2903
 
-[AID_RFS_SHARED]
+[AID_VENDOR_RFS_SHARED]
 value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
 
 [vendor/bin/wcnss_filter]
 mode: 0755
@@ -97,6 +106,18 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
@@ -121,15 +142,45 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
