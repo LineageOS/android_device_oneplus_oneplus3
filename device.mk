842b6c4ca75b063e557b10c21539b5d11d0faf66
dianlujitao <dianlujitao@lineageos.org>
2018-10-31 15:24:12 +0800

    op3: Uprev health hal to 2.0
    Change-Id: If0918993999a2f7fef235239119c97a1f01eae8a

--- device.mk
+++ device.mk
@@ -278,9 +278,8 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
