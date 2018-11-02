9fd8607f7a0addc0509a0917d5e632fc3c249be1
dianlujitao <dianlujitao@lineageos.org>
2018-11-02 13:58:46 +0800

    op3: Use vndk-27 sensorservice for slim_daemon
    Change-Id: Iec8bbc2b02118abdc5790a7bd8bc7d01e61b889d

--- extract-files.sh
+++ extract-files.sh
@@ -84,3 +84,5 @@ sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-
 
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $DEVICE_BLOB_ROOT/lib64/vendor.qti.gnss@1.0.so
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
+
+patchelf --replace-needed android.frameworks.sensorservice@1.0.so android.frameworks.sensorservice@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/bin/slim_daemon
