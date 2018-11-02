bb19d7cab8c840ef7ae1f8470a4b712ea0d4d400
dianlujitao <dianlujitao@lineageos.org>
2018-11-02 11:34:49 +0800

    op3: Restore OnePlus' logic to copy MBN from system
     * Also hax libril-qc-qmi-1.so to load MBN from /data/vendor/modem_config
    
    Change-Id: Ia702b2cdc3ad003e38a34e5a106ecfc8289e004e

--- extract-files.sh
+++ extract-files.sh
@@ -82,6 +82,13 @@ sed -i \
 sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" \
     "$DEVICE_BLOB_ROOT"/etc/permissions/qti_libpermissions.xml
 
+sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_hw\/|\/data\/vendor\/modem_config\/mcfg_hw\/\x00\x00\x00\x00\x00\x00|g" \
+    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
+sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw\/|\/data\/vendor\/modem_config\/mcfg_sw\/\x00\x00\x00\x00\x00\x00|g" \
+    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
+sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw_NoCDMA\/|\/data\/vendor\/modem_config\/mcfg_sw_NoCDMA\/\x00\x00\x00\x00\x00\x00|g" \
+    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
+
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $DEVICE_BLOB_ROOT/lib64/vendor.qti.gnss@1.0.so
 patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $DEVICE_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
 
