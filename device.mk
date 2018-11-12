689c8079be354a3b0e86c600764afe5ac4b00f44
Bruno Martins <bgcngm@gmail.com>
2018-11-12 21:18:12 +0000

    op3: Build ClearKey HIDL plugin from source
     * After all it can be built from source.
    
    Change-Id: Ie0752b3312007c94bc7dd8730fa88bd324a2ac8c

--- device.mk
+++ device.mk
@@ -248,7 +248,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
