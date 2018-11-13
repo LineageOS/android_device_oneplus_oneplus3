075e88c40896e1f6091aff58b55399699e093d9a
dianlujitao <dianlujitao@lineageos.org>
2018-11-13 15:31:28 +0800

    op3: Remove android.hardware.gnss@1.0-service-qti from product packages
     * Deprecated.
    
    Change-Id: I0e79561757bbf92bb5b7f051a05baf678d9da2ad

--- device.mk
+++ device.mk
@@ -266,8 +266,7 @@ PRODUCT_PACKAGES += \
     libcurl \
     libgnss \
     libgnsspps \
-    android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti
+    android.hardware.gnss@1.0-impl-qti
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v27.so
