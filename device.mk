daed2589a3122233906609526fdcdf3f56f01feb
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    op3: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20

--- device.mk
+++ device.mk
@@ -478,6 +478,8 @@ PRODUCT_PACKAGES += \
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
