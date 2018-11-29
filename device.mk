c984af4dd965413204d9db3cafc812af51b9b459
dianlujitao <dianlujitao@lineageos.org>
2018-11-29 14:58:50 +0800

    Revert "op3: Add android.hardware.drm@1.0-service.widevine"
     * This is a BLOB.
    
    This reverts commit 630e0703dff1b4a9cee32f3be7685d705f86ee8d.
    
    Change-Id: I3bb5247f7338dc5852c6468290a82299fdcf014c

--- device.mk
+++ device.mk
@@ -243,8 +243,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.0-service
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
