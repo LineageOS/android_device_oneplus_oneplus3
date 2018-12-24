7c98481b6a8874bb06c4476ceac5624f0579dace
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    op3: Add QCOM's WFD implementation
     * Sadly, out-of-the-box Miracast support was killed in P, so
       we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

--- device.mk
+++ device.mk
@@ -489,5 +489,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libaacwrapper \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
