67c4c0124ff3f5296b0df56e5346de22dbc5f43e
dianlujitao <dianlujitao@lineageos.org>
2018-12-26 14:21:14 +0800

    op3: Add priv-app permissions whitelist from LA.UM.7.6.r1-03900-89xx.0
    This reverts commit ef4e56b764ef5028510747ef12287ba6e50d6809.
    
    Change-Id: If4fe3b80b591d3d1f48b86bce3bca8a383dff6d7

--- device.mk
+++ device.mk
@@ -374,7 +374,7 @@ PRODUCT_COPY_FILES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # QMI
