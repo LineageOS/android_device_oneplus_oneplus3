a7ff235b5d7600375d6184df26e6a71385fe004e
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-08 13:33:43 +0100

    op3: doze: Check if AOD is enabled after boot
    * The service should not start if AOD had been turned on by the user.
    
    Change-Id: I79307adbcda57e663287714ef15830da4f285131

--- doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
+++ doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017 The LineageOS Project
+ *               2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,10 +29,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (Utils.isDozeEnabled(context) && Utils.sensorsEnabled(context)) {
-            if (DEBUG) Log.d(TAG, "Starting service");
-            Utils.startService(context);
-        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        Utils.checkDozeService(context);
     }
-
 }
