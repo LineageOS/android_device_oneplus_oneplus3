df67c73a9e8ab2837634f44d80f3dcb97f8bd403
Nvertigo <nvertigo67@gmail.com>
2019-02-04 14:56:23 +0100

    op3/t: migrate to livedisplay-2.0.
    Change-Id: I38fba93ad036aa4ac1f2a67fb67e4f7b2716b23b

--- device.mk
+++ device.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
