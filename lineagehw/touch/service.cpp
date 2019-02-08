e8afcca6b21a1f8f72eec330c0f6aa843f53cdfd
dianlujitao <dianlujitao@lineageos.org>
2019-02-08 21:23:02 +0800

    op3: Convert KeyDisabler to touch HAL
    Change-Id: Iea8c038e178a1bc72cf30bc0b345994b0f8a66fa

--- lineagehw/touch/service.cpp
+++ lineagehw/touch/service.cpp
@@ -19,18 +19,30 @@
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 
+#include "KeyDisabler.h"
 #include "TouchscreenGesture.h"
 
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::IKeyDisabler;
 using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::KeyDisabler;
 using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
 
 int main() {
-    android::sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+    sp<IKeyDisabler> keyDisabler = new KeyDisabler();
 
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    if (gestureService->registerAsService() != android::OK) {
+    if (gestureService->registerAsService() != OK) {
         LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    if (keyDisabler->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register keydisabler HAL service.";
         return 1;
     }
 
