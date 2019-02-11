6e1f0b74782ddb81c1eb1baaac0addc85b290637
dianlujitao <dianlujitao@lineageos.org>
2019-02-11 18:30:09 +0800

    op3: Move perfboostsconfig.xml to vendor
    Change-Id: Ib98817e61e2975c8fb244c78eb090d020e9a1e8e

--- device.mk
+++ device.mk
@@ -374,7 +374,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Qualcomm
