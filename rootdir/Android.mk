332ec1a9dde4af4c803d956f2aa071ceb5afcb1f
dianlujitao <dianlujitao@lineageos.org>
2019-03-16 15:23:49 +0800

    op3: Update init scripts from LA.UM.7.5.r1-04300-8x96.0
    Change-Id: I88be8416deadf5e821d33f247f934cf9673c7080

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,6 +59,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.class_late.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.class_late.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
