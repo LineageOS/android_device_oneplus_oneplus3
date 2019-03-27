eb5c6d60cf87adf9daea856d45b6ad27e17f915c
dianlujitao <dianlujitao@lineageos.org>
2019-03-27 14:04:37 +0800

    op3: Rip ANT+
     * ANT+ HIDL implementation was removed from stock Pie BT stack
     * The previous qualcomm-uart implementation is not compatible with QTI
       BT HAL so there's no way to keep it
    
    Change-Id: I33547eb7de5e77120f4acfc174ab10566070810d

--- device.mk
+++ device.mk
@@ -185,14 +185,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=rome
