b7758f251cf40302f6a68f506dfaa9f503e33642
dianlujitao <dianlujitao@lineageos.org>
2019-05-30 11:25:12 +0800

    op3: Update MBN path for P blobs
    This reverts commit 299a5ba516f22c403c633614ca51bb56add8bec6.
    
    Change-Id: I0bef764513e6aa3a05d973c87d3c17f5c68eeae2

--- extract-files.sh
+++ extract-files.sh
@@ -69,10 +69,3 @@ DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 #
 sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" \
     "$DEVICE_BLOB_ROOT"/etc/permissions/qti_libpermissions.xml
-
-sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_hw\/|\/data\/vendor\/modem_config\/mcfg_hw\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
-sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw\/|\/data\/vendor\/modem_config\/mcfg_sw\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
-sed -i "s|\/data\/vendor\/radio\/modem_config\/mcfg_sw_NoCDMA\/|\/data\/vendor\/modem_config\/mcfg_sw_NoCDMA\/\x00\x00\x00\x00\x00\x00|g" \
-    "$DEVICE_BLOB_ROOT"/vendor/lib64/libril-qc-qmi-1.so
--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -399,28 +399,24 @@ esac
 #
 # Make modem config folder and copy firmware config to that folder for RIL
 #
-#ifdef VENDOR_EDIT
-# modify the source path to /system/etc/firmware/mbn_ota/ , hanqingpu, 20151119
-##if [ -f /data/vendor/modem_config/ver_info.txt ]; then
-##    prev_version_info=`cat /data/vendor/modem_config/ver_info.txt`
-##else
-##    prev_version_info=""
-##fi
+if [ -f /data/vendor/modem_config/ver_info.txt ]; then
+    prev_version_info=`cat /data/vendor/modem_config/ver_info.txt`
+else
+    prev_version_info=""
+fi
 
-##cur_version_info=`cat /vendor/firmware_mnt/verinfo/ver_info.txt`
-##if [ ! -f /vendor/firmware_mnt/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
+cur_version_info=`cat /vendor/firmware_mnt/verinfo/ver_info.txt`
+if [ ! -f /vendor/firmware_mnt/verinfo/ver_info.txt -o "$prev_version_info" != "$cur_version_info" ]; then
     # add W for group recursively before delete
     chmod g+w -R /data/vendor/modem_config/*
     rm -rf /data/vendor/modem_config/*
     # preserve the read only mode for all subdir and files
-##    cp --preserve=m -dr /vendor/firmware_mnt/image/modem_pr/mcfg/configs/* /data/vendor/modem_config
     cp --preserve=m -dr /system/etc/firmware/mbn_ota/* /data/vendor/modem_config
-##    cp --preserve=m -d /vendor/firmware_mnt/verinfo/ver_info.txt /data/vendor/modem_config/
-##    cp --preserve=m -d /vendor/firmware_mnt/image/modem_pr/mbn_ota.txt /data/vendor/modem_config/
+    # cp --preserve=m -d /vendor/firmware_mnt/verinfo/ver_info.txt /data/vendor/modem_config/
+    # cp --preserve=m -d /vendor/firmware_mnt/image/modem_pr/mbn_ota.txt /data/vendor/modem_config/
     # the group must be root, otherwise this script could not add "W" for group recursively
     chown -hR radio.root /data/vendor/modem_config/*
-##fi
-#endif /*VENDOR_EDIT*/
+fi
 chmod g-w /data/vendor/modem_config
 setprop ro.vendor.ril.mbn_copy_completed 1
 
