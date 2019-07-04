98650cf27606f5bd00a8e29f4227653b707311bb
LuK1337 <priv.luk@gmail.com>
2019-07-04 11:51:05 +0100

    op3: pocketmode: Kill off Lineage SDK dependency
    Change-Id: I05f35124be4547b7fbc3ff3aa145bf27600454e4

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -10,18 +10,6 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v14-preference
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
