5952d4aff3fcab04f3e844c2a446a8180e657f59
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    op3: Doze: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f

--- doze/Android.mk
+++ doze/Android.mk
@@ -13,12 +13,8 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference
+    androidx.core_core \
+    androidx.preference_preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
@@ -29,5 +25,3 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
