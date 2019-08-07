0f2b27294b7d9e6cb4f6e51ac63b0537f90b24b2
Bharath Gopal <gopalb@codeaurora.org>
2019-08-07 11:57:07 +0530

    op3: Enable graphics mapper version 2.1
     Enable graphics mapper version 2.1
    
    Change-Id: I4be4782e079fec6762d012b3b07a2ea565d86efe

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
