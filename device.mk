e379e94d772dd597a65d8febcd9f06f69a2b6984
dianlujitao <dianlujitao@gmail.com>
2019-09-29 20:50:30 +0800

    op3: Move libnfc-nxp_RF.conf to /vendor
    Change-Id: I7845a7324a8eda9811f04a20b346237e624bc8de

--- device.mk
+++ device.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
