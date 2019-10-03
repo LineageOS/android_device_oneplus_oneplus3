0edef8d67e5e07307744797ec9404f54eb810b6d
Nvertigo <nvertigo67@gmail.com>
2019-10-03 17:57:59 +0200

    op3/t: update build fingerprint to OOS 9.0.5.
      Use build fingerprints from OxygenOS 9.0.5:
      OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys
      OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys
    
    Change-Id: I3f9ac4935cf94d767b2c5ed55d30d579ea1b170c

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -59,16 +59,16 @@ void load_op3(const char *model) {
     property_override_dual("ro.product.model", "ro.product.vendor.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override_dual("ro.product.device", "ro.product.vendor.device", "OnePlus3");
-    property_override("ro.build.description", "OnePlus3-user 9 PKQ1.181203.001 1905251415 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1905251415:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys");
 }
 
 void load_op3t(const char *model) {
     property_override_dual("ro.product.model", "ro.product.vendor.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override_dual("ro.product.device", "ro.product.vendor.device", "OnePlus3T");
-    property_override("ro.build.description", "OnePlus3-user 9 PKQ1.181203.001 1905250152 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1905250152:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys");
     property_set("ro.power_profile.override", "power_profile_3t");
 }
 
