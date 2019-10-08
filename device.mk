a4c36cfe499b5b73be7d0d904569f800c66da629
dianlujitao <dianlujitao@lineageos.org>
2019-10-08 21:33:32 +0800

    op3: Update CNE/DPM from LA.UM.8.6.r1-01900-89xx.0
    Change-Id: Iab80efc8d31c181bfdb0487aceaec8bb2777252f

--- device.mk
+++ device.mk
@@ -443,6 +443,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
