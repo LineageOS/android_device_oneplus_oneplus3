de97d9b7bb9553cc2ba5e776a0498ee2709e22d7
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    op3: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Encryption
 PRODUCT_PACKAGES += \
