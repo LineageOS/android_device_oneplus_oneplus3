a275007fd6af3789464c12737ac75a17febf96cc
Nvertigo <nvertigo67@gmail.com>
2019-12-24 11:20:00 +0100

    op3/t: use phone-xhdpi-6144-dalvik-heap.mk.
      Use frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk from
      265495: "Add dalvik-heap device-configs for 4/6GB devices" instead of
      OxygenOS configuration.
    
    Change-Id: Ifcfd49dbec088bccab678ff7ef2419f9744aad48

--- device.mk
+++ device.mk
@@ -81,13 +81,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+# setup dalvik vm configs.
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
