ad1e83986e048fc0f9c5e8cfbc307b756a14ce70
dianlujitao <dianlujitao@lineageos.org>
2020-01-11 16:51:31 +0800

    op3: Switch back to use Oreo build fingerprint/description
    Change-Id: If0ed41c9218d533c2ae1cffb728a5810ab4e8cc6

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -45,13 +45,13 @@ constexpr const char* RO_PROP_SOURCES[] = {
 };
 
 constexpr const char* BUILD_DESCRIPTION[] = {
-        "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys",
-        "OnePlus3-user 9 PKQ1.181203.001 1907311932 release-keys",
+        "OnePlus3-user 8.0.0 OPR1.170623.032 31 release-keys",
+        "OnePlus3-user 8.0.0 OPR1.170623.032 31 release-keys",
 };
 
 constexpr const char* BUILD_FINGERPRINT[] = {
-        "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys",
-        "OnePlus/OnePlus3/OnePlus3T:9/PKQ1.181203.001/1907311932:user/release-keys",
+        "OnePlus/OnePlus3/OnePlus3:8.0.0/OPR1.170623.032/02281230:user/release-keys",
+        "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/02281230:user/release-keys",
 };
 
 void property_override(char const prop[], char const value[]) {
