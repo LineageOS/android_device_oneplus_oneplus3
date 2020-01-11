edb4f0f984b7caa1714d92a7c3d522bae6bc09b5
dianlujitao <dianlujitao@lineageos.org>
2020-01-11 16:53:59 +0800

    op3: Update vendor SPL to match blobs
    Change-Id: If402a67fe81d11b5271d04af7516ee0c5860dcb6

--- lineage_oneplus3.mk
+++ lineage_oneplus3.mk
@@ -24,7 +24,7 @@ $(call inherit-product, device/oneplus/oneplus3/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2019-04-01
+VENDOR_SECURITY_PATCH := 2019-10-01
 
 PRODUCT_NAME := lineage_oneplus3
 PRODUCT_DEVICE := oneplus3
