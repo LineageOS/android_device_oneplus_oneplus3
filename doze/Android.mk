8b1e7996a1fdb51bdf676b76193df84db318f6b2
Bruno Martins <bgcngm@gmail.com>
2020-01-18 19:04:13 +0000

    op3: doze: Fix dark theme issues
    This brings SwitchBar widget inline with 10 and fixes the
    theme issues when the dark mode is enabled.
    
    Change-Id: I6909b77bc649eb9eb760bf01b46f66570363e9d9

--- doze/Android.mk
+++ doze/Android.mk
@@ -26,6 +26,8 @@ LOCAL_RESOURCE_DIR := \
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+include frameworks/base/packages/SettingsLib/common.mk
+
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
