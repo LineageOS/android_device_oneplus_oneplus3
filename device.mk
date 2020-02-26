859e347052a83901dd89c468940aded64243f143
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    oneplus3: Build and enable fastbootd
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     OneplusPocketMode \
