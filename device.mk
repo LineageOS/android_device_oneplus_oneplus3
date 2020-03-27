f5661f2a7e7d7e319221632c07b19471dd2ce16b
dianlujitao <dianlujitao@gmail.com>
2020-03-27 16:05:15 +0800

    op3: Import nfc config from LA.UM.8.6.r1-02900-89xx.0
    Change-Id: I06293629c16596b34916f83df148af918d169a89

--- device.mk
+++ device.mk
@@ -376,9 +376,9 @@ PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-mtp_default.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-mtp_default.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.nfc_nci=nqx.default \
