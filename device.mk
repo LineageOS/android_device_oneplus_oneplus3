b8048aec46d66b15d56757c7bb275da3de2f5e52
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:15:39 +0800

    op3: Provide shim for libdpmframework.so
    Change-Id: Ic5d732f3e98ba45c886db9646d6ef30eb0d42bf4

--- device.mk
+++ device.mk
@@ -474,6 +474,7 @@ PRODUCT_PACKAGES += \
 
 # Shims
 PRODUCT_PACKAGES += \
+    libcutils_shim \
     libui_shim
 
 # Telephony
--- libshims/Android.mk
+++ libshims/Android.mk
@@ -26,3 +26,15 @@ LOCAL_MODULE := libui_shim
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	strdup16to8.cpp \
+	strdup8to16.cpp
+
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := libcutils_shim
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
