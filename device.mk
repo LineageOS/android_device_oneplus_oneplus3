499b1d774421aea0243d4634ff0d14d286017fc8
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    op3: Add prebuilt protobuf from sdk28
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- device.mk
+++ device.mk
@@ -408,6 +408,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Protobuf
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so
+
 # Qualcomm
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect
